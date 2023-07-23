4baffb5dbcabd08e78a1b1b49eec832904b07fbc
Varun Jain <83868029+varun1455@users.noreply.github.com>
2023-07-23 23:26:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const { Server } = require("socket.io");
+var http = require('http');
+const cors = require("cors")  
+require("dotenv").config();  
+
+const app = express()
+app.use(cors())
+
+var server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://illustrious-tiramisu-35bcce.netlify.app/",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.get("/", (req, res) => {res.send("Chat using Socket.io by varun"); res.end()})
+
+io.on("connection", (socket) => {
+  console.log(socket.id)
+
+  socket.on("joinRoom", room => {
+		socket.join(room)
+  })
+
+  socket.on("newMessage", ({newMessage, room}) => {
+    io.in(room).emit("getLatestMessage", newMessage)
+  })
+
+});
+
+const port = process.env.PORT || 8000
+
+server.listen(port, console.log(`App started at port ${port}`))
